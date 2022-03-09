b052b09d582d823b6a2575d47a510cacb3015c2d
ameyayeole <90673451+ameyayeole@users.noreply.github.com>
2022-03-10 01:56:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor((Math.random()*6)+1);
+
+var randomImage1Src = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",randomImage1Src);
+
+var randomNumber2 = Math.floor((Math.random()*6)+1);
+
+var randomImage2Src = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImage2Src);
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 wins! 🚩";
+}
+else if(randomNumber1<randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+}
+else{
+  document.querySelector("h1").innerHTML = "Its a Tie! 🚩";
+}
